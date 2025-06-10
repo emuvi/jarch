c1cb056569aeb0179f8a70e0e7defd0fc7009cb0
Éverton M. Vieira <emuvi@outlook.com.br>
2025-06-10 14:48:09 -0300

    test: more scenarios

--- src/test/java/br/com/pointel/jarch/mage/WizCharsTest.java
+++ src/test/java/br/com/pointel/jarch/mage/WizCharsTest.java
@@ -14,7 +14,10 @@ public class WizCharsTest {
             new GetWordsTest("abc test123.45 def", List.of("abc", "test", "123.45", "def")),
             new GetWordsTest("abc test123.45 def 6789", List.of("abc", "test", "123.45", "def", "6789")),
             new GetWordsTest("'abc' test123", List.of("'", "abc", "'", "test", "123")),
-            new GetWordsTest("'abc'?! test123", List.of("'", "abc", "'?!", "test", "123"))
+            new GetWordsTest("'abc'?! test123", List.of("'", "abc", "'?!", "test", "123")),
+            new GetWordsTest("abc test123.45 abc/def", List.of("abc", "test", "123.45", "abc", "/", "def")),
+            new GetWordsTest("abc test123.45 abc\\def", List.of("abc", "test", "123.45", "abc", "\\", "def")),
+            new GetWordsTest("abc test123.45 abc.def", List.of("abc", "test", "123.45", "abc", ".", "def"))
         );
         for (var test : tests) {
             var result = WizChars.getWords(test.input);
