f3e72e51002afe48e3210121ccc61ddab26553fc
Éverton M. Vieira <emuvi@outlook.com.br>
2025-08-31 16:54:54 -0300

    feat: make thread efficient safe

--- src/main/java/br/com/pointel/jarch/data/Storage.java
+++ src/main/java/br/com/pointel/jarch/data/Storage.java
@@ -1,90 +1,62 @@
 package br.com.pointel.jarch.data;
 
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import org.apache.commons.dbcp2.BasicDataSource;
 import br.com.pointel.jarch.mage.WizChars;
 
 public class Storage {
 
     private final Map<String, BasicDataSource> stores;
-    private volatile Bases bases;
+    private final Bases bases;
 
     public Storage(Bases bases) {
         this.bases = bases;
         this.stores = new HashMap<>();
-        this.start(bases);
+        this.start();
     }
 
-    public synchronized void update(Bases bases) throws SQLException {
-        var included = new ArrayList<String>();
-        for (var base : bases) {
-            if (this.stores.containsKey(base.getName())) {
-                var oldSource = stores.get(base.getName());
-                if (!Objects.equals(oldSource.getUrl(), base.getUrl())
-                        || !Objects.equals(oldSource.getUserName(), base.getUser())
-                        || !Objects.equals(oldSource.getPassword(), base.getPass())) {
-                    oldSource.close();
-                    newSourceOnStore(base);
-                }
-            } else {
-                newSourceOnStore(base);
-            }
-            included.add(base.getName());
-        }
-        for (var baseName : this.stores.keySet()) {
-            if (!included.contains(baseName)) {
-                this.stores.get(baseName).close();
-                this.stores.remove(baseName);
-            }
-        }
-        this.bases = bases;
-    }
-
-    public synchronized Connection getLink(String ofBaseName) throws Exception {
+    public Connection getLink(String ofBaseName) throws Exception {
         if (this.stores == null) {
             throw new Exception("No stores are served.");
         }
-        var stored = this.stores.get(ofBaseName);
-        if (stored == null) {
+        var dataSource = this.stores.get(ofBaseName);
+        if (dataSource == null) {
             throw new Exception("Base " + ofBaseName + " not found");
         }
-        return stored.getConnection();
+        return dataSource.getConnection();
     }
 
-    public synchronized EOrm getEOrm(String onBaseName) throws Exception {
+    public EOrm getEOrm(String onBaseName) throws Exception {
         if (this.stores == null) {
             throw new Exception("No stores are served.");
         }
-        var stored = this.stores.get(onBaseName);
-        if (stored == null) {
+        var dataSource = this.stores.get(onBaseName);
+        if (dataSource == null) {
             throw new Exception("Base " + onBaseName + " not found");
         }
-        var link = stored.getConnection();
+        var link = dataSource.getConnection();
         link.setAutoCommit(true);
         var dataWay = this.bases.getFromName(onBaseName);
         var eOrmClass = dataWay.getEOrmClass();
         return eOrmClass.getConstructor(Connection.class).newInstance(link);
     }
 
-    public synchronized ESql getESql(String onBaseName) throws Exception {
+    public ESql getESql(String onBaseName) throws Exception {
         if (this.stores == null) {
             throw new Exception("No stores are served.");
         }
-        var stored = this.stores.get(onBaseName);
-        if (stored == null) {
+        var dataSource = this.stores.get(onBaseName);
+        if (dataSource == null) {
             throw new Exception("Base " + onBaseName + " not found");
         }
-        var link = stored.getConnection();
+        var link = dataSource.getConnection();
         link.setAutoCommit(true);
         return new ESql(link);
     }
 
-    private void start(Bases bases) {
+    private void start() {
         for (var base : bases) {
             newSourceOnStore(base);
         }
