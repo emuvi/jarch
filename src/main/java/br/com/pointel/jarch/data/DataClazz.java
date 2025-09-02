629683c79c5fd46c44fd403eb759e0b8314df7aa
Éverton M. Vieira <emuvi@outlook.com.br>
2025-09-02 10:12:15 -0300

    feat: data clazz

---
+++ src/main/java/br/com/pointel/jarch/data/DataClazz.java
@@ -0,0 +1,50 @@
+package br.com.pointel.jarch.data;
+
+import br.com.pointel.jarch.mage.WizData;
+
+public class DataClazz implements Data {
+
+    public String data;
+    public String clazz;
+
+    public DataClazz() {
+    }
+
+    public DataClazz(Object value) {
+        this.data = value == null ? null : WizData.toChars(value);
+        this.clazz = value == null ? null : value.getClass().getCanonicalName();
+    }
+
+    public DataClazz(String data, String clazz) {
+        this.data = data;
+        this.clazz = clazz;
+    }
+
+    public Object getValue() throws Exception {
+        return WizData.fromChars(this.data, Class.forName( this.clazz));
+    }
+
+    public <T> T getValue(Class<T> clazz) throws Exception {
+        return WizData.fromChars(this.data, clazz);
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        return this.deepEquals(that);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.deepHash();
+    }
+
+    @Override
+    public String toString() {
+        return this.toChars();
+    }
+
+    public static DataClazz fromChars(String chars) {
+        return Data.fromChars(chars, DataClazz.class);
+    }
+
+}
