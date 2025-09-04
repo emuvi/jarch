d91e098f34c1f813bbb7b94f44d25b21c248aa22
Éverton M. Vieira <emuvi@outlook.com.br>
2025-09-04 09:17:01 -0300

    feat: type heft

---
+++ src/main/java/br/com/pointel/jarch/data/TypeHeft.java
@@ -0,0 +1,32 @@
+package br.com.pointel.jarch.data;
+
+public class TypeHeft implements Firm {
+
+    public final Class<?> type;
+    public final int heft;
+
+    public TypeHeft(Class<?> type, int heft) {
+        this.type = type;
+        this.heft = heft;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        return this.deepEquals(that);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.deepHash();
+    }
+
+    @Override
+    public String toString() {
+        return this.toChars();
+    }
+
+    public static TypeHeft fromChars(String chars) {
+        return Firm.fromChars(chars, TypeHeft.class);
+    }
+
+}
