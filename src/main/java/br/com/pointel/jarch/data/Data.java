39bfc3f8af2030cabdb48cfad829c2fbad00fe25
Éverton M. Vieira <emuvi@outlook.com.br>
2025-09-05 13:53:42 -0300

    feat: data from chars

--- src/main/java/br/com/pointel/jarch/data/Data.java
+++ src/main/java/br/com/pointel/jarch/data/Data.java
@@ -1,6 +1,12 @@
 package br.com.pointel.jarch.data;
 
 import br.com.pointel.jarch.flow.FixVals;
+import br.com.pointel.jarch.mage.WizData;
 
 public interface Data extends Base, FixVals {
+
+    public static <T> T fromChars(String chars, Class<T> clazz) {
+        return WizData.fromChars(chars, clazz);
+    }
+
 }
