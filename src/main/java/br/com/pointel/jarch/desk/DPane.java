180f2546f2082fcea9ca7cf2f3021ca92a881799
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-06 06:56:53 -0300

    feat: pane border

--- src/main/java/br/com/pointel/jarch/desk/DPane.java
+++ src/main/java/br/com/pointel/jarch/desk/DPane.java
@@ -1,10 +1,13 @@
 package br.com.pointel.jarch.desk;
 
-import javax.swing.BorderFactory;
-import javax.swing.JPanel;
-
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.LayoutManager;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 public class DPane extends JPanel {
 
@@ -15,10 +18,155 @@ public class DPane extends JPanel {
         super(layout);
     }
 
+    public DPane layout(LayoutManager layout) {
+        setLayout(layout);
+        return this;
+    }
+    
+    public DPane border(Border border) {
+        setBorder(border);
+        return this;
+    }
+
     public DPane borderEmpty(int size) {
         setBorder(BorderFactory.createEmptyBorder(size, size, size, size));
         return this;
     }
+
+    public DPane borderEmpty(int top, int left, int bottom, int right) {
+        setBorder(BorderFactory.createEmptyBorder(top, left, bottom, right));
+        return this;
+    }
+
+    public DPane borderLine(Color color) {
+        setBorder(BorderFactory.createLineBorder(color));
+        return this;
+    }
+
+    public DPane borderLine(Color color, int thickness) {
+        setBorder(BorderFactory.createLineBorder(color, thickness));
+        return this;
+    }
+
+    public DPane borderLine(Color color, int thickness, boolean rounded) {
+        setBorder(BorderFactory.createLineBorder(color, thickness, rounded));
+        return this;
+    }
+
+    public DPane borderEtched() {
+        setBorder(BorderFactory.createEtchedBorder());
+        return this;
+    }
+
+    public DPane borderEtched(int type) {
+        setBorder(BorderFactory.createEtchedBorder(type));
+        return this;
+    }
+
+    public DPane borderEtched(Color highlight, Color shadow) {
+        setBorder(BorderFactory.createEtchedBorder(highlight, shadow));
+        return this;
+    }
+
+    public DPane borderEtched(int type, Color highlight, Color shadow) {
+        setBorder(BorderFactory.createEtchedBorder(type, highlight, shadow));
+        return this;
+    }
+
+    public DPane borderBevelRaised() {
+        setBorder(BorderFactory.createRaisedBevelBorder());
+        return this;
+    }
+
+    public DPane borderBevelLowered() {
+        setBorder(BorderFactory.createLoweredBevelBorder());
+        return this;
+    }
+
+    public DPane borderBevel(int type) {
+        setBorder(BorderFactory.createBevelBorder(type));
+        return this;
+    }
+
+    public DPane borderBevel(int type, Color highlight, Color shadow) {
+        setBorder(BorderFactory.createBevelBorder(type, highlight, shadow));
+        return this;
+    }
+
+    public DPane borderBevel(int type, Color highlightOuter, Color highlightInner, Color shadowOuter, Color shadowInner) {
+        setBorder(BorderFactory.createBevelBorder(type, highlightOuter, highlightInner, shadowOuter, shadowInner));
+        return this;
+    }
+
+    public DPane borderSoftBevel(int type) {
+        setBorder(BorderFactory.createSoftBevelBorder(type));
+        return this;
+    }
+
+    public DPane borderSoftBevel(int type, Color highlight, Color shadow) {
+        setBorder(BorderFactory.createSoftBevelBorder(type, highlight, shadow));
+        return this;
+    }
+
+    public DPane borderSoftBevel(int type, Color highlightOuter, Color highlightInner, Color shadowOuter, Color shadowInner) {
+        setBorder(BorderFactory.createSoftBevelBorder(type, highlightOuter, highlightInner, shadowOuter, shadowInner));
+        return this;
+    }
+
+    public DPane borderTitled(String title) {
+        setBorder(BorderFactory.createTitledBorder(title));
+        return this;
+    }
+
+    public DPane borderTitled(Border border) {
+        setBorder(BorderFactory.createTitledBorder(border));
+        return this;
+    }
+
+    public DPane borderTitled(Border border, String title) {
+        setBorder(BorderFactory.createTitledBorder(border, title));
+        return this;
+    }
+
+    public DPane borderTitled(String title, int justification, int position) {
+        setBorder(BorderFactory.createTitledBorder(null, title, justification, position));
+        return this;
+    }
+
+    public DPane borderTitled(String title, int justification, int position, Font font) {
+        setBorder(BorderFactory.createTitledBorder(null, title, justification, position, font));
+        return this;
+    }
+
+    public DPane borderTitled(String title, int justification, int position, Font font, Color color) {
+        setBorder(BorderFactory.createTitledBorder(null, title, justification, position, font, color));
+        return this;
+    }
+
+    public DPane borderTitled(Border border, String title, int justification, int position, Font font, Color color) {
+        setBorder(BorderFactory.createTitledBorder(border, title, justification, position, font, color));
+        return this;
+    }
+
+    public DPane borderMatte(int top, int left, int bottom, int right, Color color) {
+        setBorder(BorderFactory.createMatteBorder(top, left, bottom, right, color));
+        return this;
+    }
+
+    public DPane borderMatte(int top, int left, int bottom, int right, Icon tileIcon) {
+        setBorder(BorderFactory.createMatteBorder(top, left, bottom, right, tileIcon));
+        return this;
+    }
+
+    public DPane borderCompound(Border outside, Border inside) {
+        setBorder(BorderFactory.createCompoundBorder(outside, inside));
+        return this;
+    }
+
+    public DPane put(DEdit<?> edit) {
+        return put(edit.comp());
+    }
+    
 
     public DPane put(Component comp) {
         add(comp);
