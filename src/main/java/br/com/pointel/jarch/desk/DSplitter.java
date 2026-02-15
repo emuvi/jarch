d497784b8ead559b442ca503342c7097e197fa68
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-15 17:06:28 -0300

    feat: border

--- src/main/java/br/com/pointel/jarch/desk/DSplitter.java
+++ src/main/java/br/com/pointel/jarch/desk/DSplitter.java
@@ -1,7 +1,13 @@
 package br.com.pointel.jarch.desk;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
 import javax.swing.JSplitPane;
+import javax.swing.border.Border;
 
 public class DSplitter extends JSplitPane {
 
@@ -102,6 +108,146 @@ public class DSplitter extends JSplitPane {
 
     public DSplitter oneTouchExpandable(boolean newValue) {
         setOneTouchExpandable(newValue);
+        return this;
+    }
+
+    public DSplitter border(Border border) {
+        setBorder(border);
+        return this;
+    }
+
+    public DSplitter borderEmpty(int size) {
+        setBorder(BorderFactory.createEmptyBorder(size, size, size, size));
+        return this;
+    }
+
+    public DSplitter borderEmpty(int top, int left, int bottom, int right) {
+        setBorder(BorderFactory.createEmptyBorder(top, left, bottom, right));
+        return this;
+    }
+
+    public DSplitter borderLine(Color color) {
+        setBorder(BorderFactory.createLineBorder(color));
+        return this;
+    }
+
+    public DSplitter borderLine(Color color, int thickness) {
+        setBorder(BorderFactory.createLineBorder(color, thickness));
+        return this;
+    }
+
+    public DSplitter borderLine(Color color, int thickness, boolean rounded) {
+        setBorder(BorderFactory.createLineBorder(color, thickness, rounded));
+        return this;
+    }
+
+    public DSplitter borderEtched() {
+        setBorder(BorderFactory.createEtchedBorder());
+        return this;
+    }
+
+    public DSplitter borderEtched(int type) {
+        setBorder(BorderFactory.createEtchedBorder(type));
+        return this;
+    }
+
+    public DSplitter borderEtched(Color highlight, Color shadow) {
+        setBorder(BorderFactory.createEtchedBorder(highlight, shadow));
+        return this;
+    }
+
+    public DSplitter borderEtched(int type, Color highlight, Color shadow) {
+        setBorder(BorderFactory.createEtchedBorder(type, highlight, shadow));
+        return this;
+    }
+
+    public DSplitter borderBevelRaised() {
+        setBorder(BorderFactory.createRaisedBevelBorder());
+        return this;
+    }
+
+    public DSplitter borderBevelLowered() {
+        setBorder(BorderFactory.createLoweredBevelBorder());
+        return this;
+    }
+
+    public DSplitter borderBevel(int type) {
+        setBorder(BorderFactory.createBevelBorder(type));
+        return this;
+    }
+
+    public DSplitter borderBevel(int type, Color highlight, Color shadow) {
+        setBorder(BorderFactory.createBevelBorder(type, highlight, shadow));
+        return this;
+    }
+
+    public DSplitter borderBevel(int type, Color highlightOuter, Color highlightInner, Color shadowOuter, Color shadowInner) {
+        setBorder(BorderFactory.createBevelBorder(type, highlightOuter, highlightInner, shadowOuter, shadowInner));
+        return this;
+    }
+
+    public DSplitter borderSoftBevel(int type) {
+        setBorder(BorderFactory.createSoftBevelBorder(type));
+        return this;
+    }
+
+    public DSplitter borderSoftBevel(int type, Color highlight, Color shadow) {
+        setBorder(BorderFactory.createSoftBevelBorder(type, highlight, shadow));
+        return this;
+    }
+
+    public DSplitter borderSoftBevel(int type, Color highlightOuter, Color highlightInner, Color shadowOuter, Color shadowInner) {
+        setBorder(BorderFactory.createSoftBevelBorder(type, highlightOuter, highlightInner, shadowOuter, shadowInner));
+        return this;
+    }
+
+    public DSplitter borderTitled(String title) {
+        setBorder(BorderFactory.createTitledBorder(title));
+        return this;
+    }
+
+    public DSplitter borderTitled(Border border) {
+        setBorder(BorderFactory.createTitledBorder(border));
+        return this;
+    }
+
+    public DSplitter borderTitled(Border border, String title) {
+        setBorder(BorderFactory.createTitledBorder(border, title));
+        return this;
+    }
+
+    public DSplitter borderTitled(String title, int justification, int position) {
+        setBorder(BorderFactory.createTitledBorder(null, title, justification, position));
+        return this;
+    }
+
+    public DSplitter borderTitled(String title, int justification, int position, Font font) {
+        setBorder(BorderFactory.createTitledBorder(null, title, justification, position, font));
+        return this;
+    }
+
+    public DSplitter borderTitled(String title, int justification, int position, Font font, Color color) {
+        setBorder(BorderFactory.createTitledBorder(null, title, justification, position, font, color));
+        return this;
+    }
+
+    public DSplitter borderTitled(Border border, String title, int justification, int position, Font font, Color color) {
+        setBorder(BorderFactory.createTitledBorder(border, title, justification, position, font, color));
+        return this;
+    }
+
+    public DSplitter borderMatte(int top, int left, int bottom, int right, Color color) {
+        setBorder(BorderFactory.createMatteBorder(top, left, bottom, right, color));
+        return this;
+    }
+
+    public DSplitter borderMatte(int top, int left, int bottom, int right, Icon tileIcon) {
+        setBorder(BorderFactory.createMatteBorder(top, left, bottom, right, tileIcon));
+        return this;
+    }
+
+    public DSplitter borderCompound(Border outside, Border inside) {
+        setBorder(BorderFactory.createCompoundBorder(outside, inside));
         return this;
     }
 
