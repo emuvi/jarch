4a0bf58f7cd1799f550fd07e8ce6ce046f33167d
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-16 13:54:12 -0300

    feat: fluent windows functions

--- src/main/java/br/com/pointel/jarch/desk/DFrame.java
+++ src/main/java/br/com/pointel/jarch/desk/DFrame.java
@@ -1,6 +1,9 @@
 package br.com.pointel.jarch.desk;
 
 import java.awt.Container;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.function.Consumer;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
@@ -40,6 +43,106 @@ public class DFrame extends JFrame {
     public DFrame body(Container body) {
         setContentPane(body);
         pack();
+        return this;
+    }
+
+    public DFrame onOpened(Consumer<WindowEvent> consumer) {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                consumer.accept(e);
+            }
+        });
+        return this;
+    }
+
+    public DFrame onClosing(Consumer<WindowEvent> consumer) {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                consumer.accept(e);
+            }
+        });
+        return this;
+    }
+
+    public DFrame onClosed(Consumer<WindowEvent> consumer) {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                consumer.accept(e);
+            }
+        });
+        return this;
+    }
+
+    public DFrame onIconified(Consumer<WindowEvent> consumer) {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowIconified(WindowEvent e) {
+                consumer.accept(e);
+            }
+        });
+        return this;
+    }
+
+    public DFrame onDeiconified(Consumer<WindowEvent> consumer) {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+                consumer.accept(e);
+            }
+        });
+        return this;
+    }
+
+    public DFrame onActivated(Consumer<WindowEvent> consumer) {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowActivated(WindowEvent e) {
+                consumer.accept(e);
+            }
+        });
+        return this;
+    }
+
+    public DFrame onDeactivated(Consumer<WindowEvent> consumer) {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+                consumer.accept(e);
+            }
+        });
+        return this;
+    }
+
+    public DFrame onStateChanged(Consumer<WindowEvent> consumer) {
+        addWindowStateListener(new WindowAdapter() {
+            @Override
+            public void windowStateChanged(WindowEvent e) {
+                consumer.accept(e);
+            }
+        });
+        return this;
+    }
+
+    public DFrame onGainedFocus(Consumer<WindowEvent> consumer) {
+        addWindowFocusListener(new WindowAdapter() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                consumer.accept(e);
+            }
+        });
+        return this;
+    }
+
+    public DFrame onLostFocus(Consumer<WindowEvent> consumer) {
+        addWindowFocusListener(new WindowAdapter() {
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                consumer.accept(e);
+            }
+        });
         return this;
     }
 
