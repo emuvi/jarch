a10a300524d36f3ed8482c8930cb5694b70af7f3
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-17 18:31:27 -0300

    feat: alignment fluent functions

--- src/main/java/br/com/pointel/jarch/desk/DFieldEdit.java
+++ src/main/java/br/com/pointel/jarch/desk/DFieldEdit.java
@@ -34,4 +34,38 @@ public abstract class DFieldEdit<T> extends DEdit<T> {
         return this;
     }
 
+    public int horizontalAlignment() {
+        return comp().getHorizontalAlignment();
+    }
+
+    public DFieldEdit<T> horizontalAlignment(int alignment) {
+        comp().setHorizontalAlignment(alignment);
+        return this;
+    }
+
+    public DFieldEdit<T> horizontalAlignmentLeft() {
+        comp().setHorizontalAlignment(JTextField.LEFT);
+        return this;
+    }
+
+    public DFieldEdit<T> horizontalAlignmentCenter() {
+        comp().setHorizontalAlignment(JTextField.CENTER);
+        return this;
+    }
+
+    public DFieldEdit<T> horizontalAlignmentRight() {
+        comp().setHorizontalAlignment(JTextField.RIGHT);
+        return this;
+    }
+
+    public DFieldEdit<T> horizontalAlignmentLeading() {
+        comp().setHorizontalAlignment(JTextField.LEADING);
+        return this;
+    }
+
+    public DFieldEdit<T> horizontalAlignmentTrailing() {
+        comp().setHorizontalAlignment(JTextField.TRAILING);
+        return this;
+    }
+
 }
