88db1cffa174d642284b1dd6ed590fe2275d2293
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-17 18:39:21 -0300

    feat: prevent not integer characters

--- src/main/java/br/com/pointel/jarch/desk/DIntegerField.java
+++ src/main/java/br/com/pointel/jarch/desk/DIntegerField.java
@@ -1,5 +1,10 @@
 package br.com.pointel.jarch.desk;
 
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+
 public class DIntegerField extends DFieldEdit<Integer> {
 
     public DIntegerField() {
@@ -8,15 +13,67 @@ public class DIntegerField extends DFieldEdit<Integer> {
 
     public DIntegerField(Integer value) {
         super(value == null ? "" : value.toString());
+        ((AbstractDocument) comp().getDocument()).setDocumentFilter(new IntegerFilter());
     }
 
+    @Override
     public Integer getValue() {
-        return comp().getText() == null || comp().getText().isEmpty() 
-                ? null : Integer.parseInt(comp().getText());
+        var text = comp().getText();
+        if (text == null || text.isEmpty() || "-".equals(text)) {
+            return null;
+        }
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 
+    @Override
     public void setValue(Integer value) {
         comp().setText(value == null ? "" : value.toString());
+    }
+
+    private class IntegerFilter extends DocumentFilter {
+        
+        @Override
+        public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+            var text = fb.getDocument().getText(0, fb.getDocument().getLength());
+            var newText = text.substring(0, offset) + string + text.substring(offset);
+            if (isValidInteger(newText)) {
+                super.insertString(fb, offset, string, attr);
+            }
+        }
+
+        @Override
+        public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+            var currentText = fb.getDocument().getText(0, fb.getDocument().getLength());
+            var newText = currentText.substring(0, offset) + text + currentText.substring(offset + length);
+            if (isValidInteger(newText)) {
+                super.replace(fb, offset, length, text, attrs);
+            }
+        }
+
+        @Override
+        public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
+            var currentText = fb.getDocument().getText(0, fb.getDocument().getLength());
+            var newText = currentText.substring(0, offset) + currentText.substring(offset + length);
+            if (isValidInteger(newText)) {
+                super.remove(fb, offset, length);
+            }
+        }
+
+        private boolean isValidInteger(String text) {
+            if (text.isEmpty() || "-".equals(text)) {
+                return true;
+            }
+            try {
+                Integer.parseInt(text);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
     }
 
 }
