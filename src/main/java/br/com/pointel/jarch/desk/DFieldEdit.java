9bed31a8b9d5774a28fa8c65bc2fd0f714b15302
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-18 10:23:13 -0300

    feat: helper functions

--- src/main/java/br/com/pointel/jarch/desk/DFieldEdit.java
+++ src/main/java/br/com/pointel/jarch/desk/DFieldEdit.java
@@ -34,6 +34,117 @@ public abstract class DFieldEdit<T> extends DEdit<T> {
         return this;
     }
 
+    public boolean editable() {
+        return comp().isEditable();
+    }
+
+    public DFieldEdit<T> editable(boolean editable) {
+        comp().setEditable(editable);
+        return this;
+    }
+
+    public boolean enabled() {
+        return comp().isEnabled();
+    }
+
+    public DFieldEdit<T> enabled(boolean enabled) {
+        comp().setEnabled(enabled);
+        return this;
+    }
+
+    @Override
+    public T value() {
+        return getValue();
+    }
+
+    @Override
+    public DFieldEdit<T> value(T value) {
+        setValue(value);
+        return this;
+    }
+
+    @Override
+    public String name() {
+        return comp().getName();
+    }
+
+    @Override
+    public DFieldEdit<T> name(String name) {
+        comp().setName(name);
+        return this;
+    }
+
+    @Override
+    public String hint() {
+        return comp().getToolTipText();
+    }
+
+    @Override
+    public DFieldEdit<T> hint(String hint) {
+        comp().setToolTipText(hint);
+        return this;
+    }
+
+    public boolean focusable() {
+        return comp().isFocusable();
+    }
+
+    public DFieldEdit<T> focusable(boolean focusable) {
+        comp().setFocusable(focusable);
+        return this;
+    }
+
+    public DFieldEdit<T> select(int selectionStart, int selectionEnd) {
+        comp().select(selectionStart, selectionEnd);
+        return this;
+    }
+
+    public DFieldEdit<T> selectAll() {
+        comp().selectAll();
+        return this;
+    }
+
+    public int selectionStart() {
+        return comp().getSelectionStart();
+    }
+
+    public DFieldEdit<T> selectionStart(int selectionStart) {
+        comp().setSelectionStart(selectionStart);
+        return this;
+    }
+
+    public int selectionEnd() {
+        return comp().getSelectionEnd();
+    }
+
+    public DFieldEdit<T> selectionEnd(int selectionEnd) {
+        comp().setSelectionEnd(selectionEnd);
+        return this;
+    }
+
+    public String selectedText() {
+        return comp().getSelectedText();
+    }
+
+    public DFieldEdit<T> replaceSelection(String content) {
+        comp().replaceSelection(content);
+        return this;
+    }
+
+    public int caretPosition() {
+        return comp().getCaretPosition();
+    }
+
+    public DFieldEdit<T> caretPosition(int position) {
+        comp().setCaretPosition(position);
+        return this;
+    }
+
+    public DFieldEdit<T> moveCaretPosition(int position) {
+        comp().moveCaretPosition(position);
+        return this;
+    }
+
     public int horizontalAlignment() {
         return comp().getHorizontalAlignment();
     }
