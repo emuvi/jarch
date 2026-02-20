9c27d814409d794897e0463c40da3d41a574292f
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-20 10:17:45 -0300

    feat: fluent functions

--- src/main/java/br/com/pointel/jarch/desk/DFrame.java
+++ src/main/java/br/com/pointel/jarch/desk/DFrame.java
@@ -1,6 +1,10 @@
 package br.com.pointel.jarch.desk;
 
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.function.Consumer;
@@ -35,14 +39,158 @@ public class DFrame extends JFrame {
         WizGUI.initFrame(this);
     }
 
-    public DFrame exitOnClose() {
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        return this;
-    }
-
     public DFrame body(Container body) {
         setContentPane(body);
         pack();
+        return this;
+    }
+
+    public String title() {
+        return super.getTitle();
+    }
+
+    public DFrame title(String title) {
+        super.setTitle(title);
+        return this;
+    }
+
+    public Dimension dimension() {
+        return super.getSize();
+    }
+
+    public DFrame dimension(int width, int height) {
+        super.setSize(width, height);
+        return this;
+    }
+
+    public boolean visible() {
+        return super.isVisible();
+    }
+
+    public DFrame visible(boolean visible) {
+        super.setVisible(visible);
+        return this;
+    }
+
+    public boolean resizable() {
+        return super.isResizable();
+    }
+
+    public DFrame resizable(boolean resizable) {
+        super.setResizable(resizable);
+        return this;
+    }
+
+    public boolean undecorated() {
+        return super.isUndecorated();
+    }
+
+    public DFrame undecorated(boolean undecorated) {
+        super.setUndecorated(undecorated);
+        return this;
+    }
+
+    public boolean alwaysOnTop() {
+        return super.isAlwaysOnTop();
+    }
+
+    public DFrame alwaysOnTop(boolean alwaysOnTop) {
+        super.setAlwaysOnTop(alwaysOnTop);
+        return this;
+    }
+
+    public Point location() {
+        return super.getLocation();
+    }
+
+    public DFrame location(int x, int y) {
+        super.setLocation(x, y);
+        return this;
+    }
+
+    public DFrame location(Point p) {
+        super.setLocation(p);
+        return this;
+    }
+
+    public Rectangle bounds() {
+        return super.getBounds();
+    }
+
+    public DFrame bounds(int x, int y, int width, int height) {
+        super.setBounds(x, y, width, height);
+        return this;
+    }
+
+    public DFrame bounds(Rectangle r) {
+        super.setBounds(r);
+        return this;
+    }
+
+    public int state() {
+        return super.getExtendedState();
+    }
+
+    public DFrame state(int state) {
+        super.setExtendedState(state);
+        return this;
+    }
+
+    public DFrame stateNormal() {
+        return state(JFrame.NORMAL);
+    }
+
+    public DFrame stateIconified() {
+        return state(JFrame.ICONIFIED);
+    }
+
+    public DFrame stateMaximizedHorizontal() {
+        return state(JFrame.MAXIMIZED_HORIZ);
+    }
+
+    public DFrame stateMaximizedVertical() {
+        return state(JFrame.MAXIMIZED_VERT);
+    }
+
+    public DFrame stateMaximizedBoth() {
+        return state(JFrame.MAXIMIZED_BOTH);
+    }
+
+    public int defaultCloseOperation() {
+        return super.getDefaultCloseOperation();
+    }
+
+    public DFrame defaultCloseOperation(int operation) {
+        super.setDefaultCloseOperation(operation);
+        return this;
+    }
+
+    public DFrame doNothingOnClose() {
+        super.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        return this;
+    }
+
+    public DFrame hideOnClose() {
+        super.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        return this;
+    }
+
+    public DFrame disposeOnClose() {
+        super.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        return this;
+    }
+
+    public DFrame exitOnClose() {
+        super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        return this;
+    }
+
+    public Image iconImage() {
+        return super.getIconImage();
+    }
+
+    public DFrame iconImage(Image image) {
+        super.setIconImage(image);
         return this;
     }
 
