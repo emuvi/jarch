333c46e76c537df1747ed90e0fc85f868dcad2de
Éverton M. Vieira <emuvi@outlook.com.br>
2026-07-11 18:10:59 -0300

    feat: add FTP client utility class with integrated Pace logging support

--- src/main/java/com/vidlus/jarch/flow/FTP.java
+++ src/main/java/com/vidlus/jarch/flow/FTP.java
@@ -1,12 +1,16 @@
 package com.vidlus.jarch.flow;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
@@ -329,6 +333,255 @@ public class FTP implements AutoCloseable {
             // Ignore
         }
         return false;
+    }
+
+    /**
+     * Streams data from an input stream up to a remote path.
+     *
+     * @param input  the source input stream to read from
+     * @param remote the target filename on the FTP server
+     * @throws Exception if an I/O error occurs
+     */
+    public void upload(InputStream input, String remote) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Uploading stream to " + remote);
+        if (!client.storeFile(remote, input)) {
+            throw new Exception("Could not upload stream.");
+        }
+    }
+
+    /**
+     * Streams a remote file down to an output stream.
+     *
+     * @param remote the source filename on the FTP server
+     * @param output the target output stream to write into
+     * @throws Exception if an I/O error occurs
+     */
+    public void download(String remote, OutputStream output) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Downloading " + remote + " to stream");
+        if (!client.retrieveFile(remote, output)) {
+            throw new Exception("Could not download file.");
+        }
+    }
+
+    /**
+     * Uploads a String to a remote path.
+     *
+     * @param content the String content to upload
+     * @param remote  the target filename on the FTP server
+     * @throws Exception if an I/O error occurs
+     */
+    public void uploadString(String content, String remote) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Uploading String to " + remote);
+        try (InputStream input = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8))) {
+            if (!client.storeFile(remote, input)) {
+                throw new Exception("Could not upload String.");
+            }
+        }
+    }
+
+    /**
+     * Downloads a remote file as a String.
+     *
+     * @param remote the source filename on the FTP server
+     * @return the content of the remote file as a String
+     * @throws Exception if an I/O error occurs
+     */
+    public String downloadString(String remote) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Downloading " + remote + " as String");
+        try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
+            if (!client.retrieveFile(remote, output)) {
+                throw new Exception("Could not download file to String.");
+            }
+            return output.toString(StandardCharsets.UTF_8);
+        }
+    }
+
+    /**
+     * Appends a local file to a remote file.
+     *
+     * @param local  the physical file to append
+     * @param remote the target filename on the FTP server
+     * @throws Exception if an I/O error occurs
+     */
+    public void append(File local, String remote) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Appending " + local.getName() + " to " + remote);
+        try (InputStream input = new FileInputStream(local)) {
+            if (!client.appendFile(remote, input)) {
+                throw new Exception("Could not append file.");
+            }
+        }
+    }
+
+    /**
+     * Appends data from an input stream to a remote file.
+     *
+     * @param input  the source input stream to read from
+     * @param remote the target filename on the FTP server
+     * @throws Exception if an I/O error occurs
+     */
+    public void append(InputStream input, String remote) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Appending stream to " + remote);
+        if (!client.appendFile(remote, input)) {
+            throw new Exception("Could not append stream.");
+        }
+    }
+
+    /**
+     * Appends a String to a remote file.
+     *
+     * @param content the String content to append
+     * @param remote  the target filename on the FTP server
+     * @throws Exception if an I/O error occurs
+     */
+    public void appendString(String content, String remote) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Appending String to " + remote);
+        try (InputStream input = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8))) {
+            if (!client.appendFile(remote, input)) {
+                throw new Exception("Could not append String.");
+            }
+        }
+    }
+
+    /**
+     * Retrieves the size of a remote file.
+     *
+     * @param remote the filename on the FTP server
+     * @return the size of the file in bytes, or -1 if the file does not exist
+     * @throws Exception if an I/O error occurs
+     */
+    public long getSize(String remote) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Getting size of " + remote);
+        FTPFile[] files = client.listFiles(remote);
+        if (files != null && files.length > 0) {
+            return files[0].getSize();
+        }
+        return -1;
+    }
+
+    /**
+     * Retrieves the modification time of a remote file.
+     *
+     * @param remote the filename on the FTP server
+     * @return the modification time as a Calendar object, or null if it cannot be retrieved
+     * @throws Exception if an I/O error occurs
+     */
+    public Calendar getModificationTime(String remote) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Getting modification time of " + remote);
+        FTPFile[] files = client.listFiles(remote);
+        if (files != null && files.length > 0) {
+            return files[0].getTimestamp();
+        }
+        return null;
+    }
+
+    /**
+     * Sends a NOOP command to the FTP server. This is useful for keeping the connection alive.
+     *
+     * @throws Exception if an I/O error occurs
+     */
+    public void sendNoOp() throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Sending NOOP");
+        if (!client.sendNoOp()) {
+            throw new Exception("Could not send NOOP.");
+        }
+    }
+
+    /**
+     * Recursively deletes a remote directory and all its contents.
+     *
+     * @param path the remote directory path
+     * @throws Exception if an I/O error occurs or a file/directory cannot be deleted
+     */
+    public void removeDirRecursively(String path) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Recursively removing directory " + path);
+        List<FTPFile> files = listFiles(path);
+        for (FTPFile file : files) {
+            if (file.getName().equals(".") || file.getName().equals("..")) {
+                continue;
+            }
+            String filePath = path.endsWith("/") ? path + file.getName() : path + "/" + file.getName();
+            if (file.isDirectory()) {
+                removeDirRecursively(filePath);
+            } else {
+                removeFile(filePath);
+            }
+        }
+        removeDir(path);
+    }
+
+    /**
+     * Recursively uploads a local directory to a remote directory.
+     *
+     * @param localDir  the local directory to upload
+     * @param remoteDir the target remote directory
+     * @throws Exception if an I/O error occurs
+     */
+    public void uploadDirRecursively(File localDir, String remoteDir) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Recursively uploading directory " + localDir.getName() + " to " + remoteDir);
+        
+        String currentDir = printWorkingDirectory();
+        
+        if (!exists(remoteDir)) {
+            openWithMakeDir(remoteDir);
+        }
+        changeDir(currentDir);
+
+        File[] files = localDir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                String remoteFilePath = remoteDir.endsWith("/") ? remoteDir + file.getName() : remoteDir + "/" + file.getName();
+                if (file.isDirectory()) {
+                    uploadDirRecursively(file, remoteFilePath);
+                } else {
+                    upload(file, remoteFilePath);
+                }
+            }
+        }
+    }
+
+    /**
+     * Recursively downloads a remote directory to a local directory.
+     *
+     * @param remoteDir the remote directory to download
+     * @param localDir  the target local directory
+     * @throws Exception if an I/O error occurs
+     */
+    public void downloadDirRecursively(String remoteDir, File localDir) throws Exception {
+        pace.waitIfPausedAndThrowIfStopped();
+        pace.info("Recursively downloading directory " + remoteDir + " to " + localDir.getName());
+
+        if (!localDir.exists()) {
+            if (!localDir.mkdirs()) {
+                throw new Exception("Could not create local directory: " + localDir.getAbsolutePath());
+            }
+        }
+
+        List<FTPFile> files = listFiles(remoteDir);
+        for (FTPFile file : files) {
+            if (file.getName().equals(".") || file.getName().equals("..")) {
+                continue;
+            }
+            String remoteFilePath = remoteDir.endsWith("/") ? remoteDir + file.getName() : remoteDir + "/" + file.getName();
+            File localFile = new File(localDir, file.getName());
+
+            if (file.isDirectory()) {
+                downloadDirRecursively(remoteFilePath, localFile);
+            } else {
+                download(remoteFilePath, localFile);
+            }
+        }
     }
 
     /**
