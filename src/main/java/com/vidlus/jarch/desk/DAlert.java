80eec31361104ac109aabb1d9a93c1d3374d1b77
Éverton M. Vieira <emuvi@outlook.com.br>
2026-07-11 20:34:56 -0300

    feat: add DAlert class for fluent JOptionPane dialog construction

--- src/main/java/com/vidlus/jarch/desk/DAlert.java
+++ src/main/java/com/vidlus/jarch/desk/DAlert.java
@@ -18,15 +18,31 @@ public class DAlert {
     private Object[] options;
     private Object initialValue;
 
+    /**
+     * Constructs a new DAlert with a default title of "Message".
+     */
     public DAlert() {
         this.title = "Message";
     }
 
+    /**
+     * Sets the parent component for the dialog.
+     * The dialog will be centered relative to this parent.
+     * 
+     * @param parent the parent Component
+     * @return this DAlert instance
+     */
     public DAlert parent(Component parent) {
         this.parent = parent;
         return this;
     }
 
+    /**
+     * Sets the main message to display in the dialog.
+     * 
+     * @param message the message object (usually a String or Component)
+     * @return this DAlert instance
+     */
     public DAlert message(Object message) {
         this.message = message;
         return this;
@@ -40,51 +56,103 @@ public class DAlert {
         return this;
     }
 
+    /**
+     * Sets the title of the dialog window.
+     * 
+     * @param title the dialog title
+     * @return this DAlert instance
+     */
     public DAlert title(String title) {
         this.title = title;
         return this;
     }
 
+    /**
+     * Configures the dialog as an Information message type.
+     * 
+     * @return this DAlert instance
+     */
     public DAlert info() {
         this.messageType = JOptionPane.INFORMATION_MESSAGE;
         return this;
     }
 
+    /**
+     * Configures the dialog as a Warning message type.
+     * 
+     * @return this DAlert instance
+     */
     public DAlert warning() {
         this.messageType = JOptionPane.WARNING_MESSAGE;
         return this;
     }
 
+    /**
+     * Configures the dialog as an Error message type.
+     * 
+     * @return this DAlert instance
+     */
     public DAlert error() {
         this.messageType = JOptionPane.ERROR_MESSAGE;
         return this;
     }
 
+    /**
+     * Configures the dialog as a Question message type.
+     * 
+     * @return this DAlert instance
+     */
     public DAlert question() {
         this.messageType = JOptionPane.QUESTION_MESSAGE;
         return this;
     }
 
+    /**
+     * Configures the dialog as a Plain message type (no default icon).
+     * 
+     * @return this DAlert instance
+     */
     public DAlert plain() {
         this.messageType = JOptionPane.PLAIN_MESSAGE;
         return this;
     }
 
+    /**
+     * Configures the dialog to display Yes and No buttons.
+     * 
+     * @return this DAlert instance
+     */
     public DAlert yesNo() {
         this.optionType = JOptionPane.YES_NO_OPTION;
         return this;
     }
 
+    /**
+     * Configures the dialog to display Yes, No, and Cancel buttons.
+     * 
+     * @return this DAlert instance
+     */
     public DAlert yesNoCancel() {
         this.optionType = JOptionPane.YES_NO_CANCEL_OPTION;
         return this;
     }
 
+    /**
+     * Configures the dialog to display OK and Cancel buttons.
+     * 
+     * @return this DAlert instance
+     */
     public DAlert okCancel() {
         this.optionType = JOptionPane.OK_CANCEL_OPTION;
         return this;
     }
 
+    /**
+     * Sets a custom icon to display in the dialog.
+     * 
+     * @param icon the custom Icon
+     * @return this DAlert instance
+     */
     public DAlert icon(Icon icon) {
         this.icon = icon;
         return this;
@@ -98,6 +166,12 @@ public class DAlert {
         return this;
     }
 
+    /**
+     * Sets the initially selected value (used primarily for input dialogs).
+     * 
+     * @param initialValue the default selection
+     * @return this DAlert instance
+     */
     public DAlert initialValue(Object initialValue) {
         this.initialValue = initialValue;
         return this;
