d677e3c15207b3afdea6b85f5fa65defb454cb2f
Éverton M. Vieira <emuvi@outlook.com.br>
2026-07-12 18:06:07 -0300

    feat: add DEditChange abstract class for text fields with integrated action buttons

--- src/main/java/com/vidlus/jarch/desk/DEditChange.java
+++ src/main/java/com/vidlus/jarch/desk/DEditChange.java
@@ -20,14 +20,27 @@ import javax.swing.UIManager;
  */
 public abstract class DEditChange<T> extends DEdit<T> {
 
+    /** The main panel that encapsulates the field and button. */
     protected JPanel panel;
+    
+    /** The text field used for displaying or editing the string representation of the value. */
     protected JTextField field;
+    
+    /** The button that triggers the action to change the value, such as opening a dialog. */
     protected JButton actionButton;
 
+    /**
+     * Constructs a new DEditChange with a default button text "*".
+     */
     public DEditChange() {
         this("*");
     }
 
+    /**
+     * Constructs a new DEditChange with the specified text for the action button.
+     * 
+     * @param buttonText the text to display on the action button
+     */
     public DEditChange(String buttonText) {
         super(new JPanel(new BorderLayout()));
         this.panel = (JPanel) super.comp();
@@ -60,6 +73,11 @@ public abstract class DEditChange<T> extends DEdit<T> {
         });
     }
 
+    /**
+     * Returns the underlying panel component that contains both the text field and the action button.
+     * 
+     * @return the main panel component
+     */
     @Override
     public JComponent comp() {
         return panel;
@@ -87,7 +105,7 @@ public abstract class DEditChange<T> extends DEdit<T> {
      * Set the number of columns in this field.
      * 
      * @param cols the number of columns
-     * @return This DEditFieldAction instance.
+     * @return This DEditChange instance.
      */
     public DEditChange<T> cols(int cols) {
         field.setColumns(cols);
@@ -103,13 +121,14 @@ public abstract class DEditChange<T> extends DEdit<T> {
         return field.getColumns();
     }
 
+    /** A flag indicating whether the field and button are currently editable. */
     protected boolean isEditable = true;
 
     /**
      * Sets whether this field is editable.
      * 
      * @param editable true if the field is editable
-     * @return This DEditFieldAction instance.
+     * @return This DEditChange instance.
      */
     public DEditChange<T> editable(boolean editable) {
         this.isEditable = editable;
@@ -134,11 +153,240 @@ public abstract class DEditChange<T> extends DEdit<T> {
      * Changes the text on the action button.
      * 
      * @param text the button text
-     * @return This DEditFieldAction instance.
+     * @return This DEditChange instance.
      */
     public DEditChange<T> buttonText(String text) {
         actionButton.setText(text);
         return this;
+    }
+
+    /**
+     * Returns the text on the action button.
+     * 
+     * @return the button text
+     */
+    public String buttonText() {
+        return actionButton.getText();
+    }
+
+    /**
+     * Sets the icon on the action button.
+     * 
+     * @param icon the button icon
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> buttonIcon(javax.swing.Icon icon) {
+        actionButton.setIcon(icon);
+        return this;
+    }
+
+    /**
+     * Returns the icon on the action button.
+     * 
+     * @return the button icon
+     */
+    public javax.swing.Icon buttonIcon() {
+        return actionButton.getIcon();
+    }
+
+    /**
+     * Sets the tooltip text on the action button.
+     * 
+     * @param text the tooltip text
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> buttonTooltip(String text) {
+        actionButton.setToolTipText(text);
+        return this;
+    }
+
+    /**
+     * Returns the tooltip text on the action button.
+     * 
+     * @return the button tooltip text
+     */
+    public String buttonTooltip() {
+        return actionButton.getToolTipText();
+    }
+
+    /**
+     * Sets the font on the action button.
+     * 
+     * @param font the button font
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> buttonFont(java.awt.Font font) {
+        actionButton.setFont(font);
+        return this;
+    }
+
+    /**
+     * Returns the font on the action button.
+     * 
+     * @return the button font
+     */
+    public java.awt.Font buttonFont() {
+        return actionButton.getFont();
+    }
+
+    /**
+     * Sets whether the action button is visible.
+     * 
+     * @param visible true if the button should be visible
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> buttonVisible(boolean visible) {
+        actionButton.setVisible(visible);
+        return this;
+    }
+
+    /**
+     * Returns whether the action button is visible.
+     * 
+     * @return true if the button is visible
+     */
+    public boolean buttonVisible() {
+        return actionButton.isVisible();
+    }
+
+    /**
+     * Sets the text of the inner text field.
+     * 
+     * @param text the text
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> text(String text) {
+        field.setText(text);
+        return this;
+    }
+
+    /**
+     * Returns the text of the inner text field.
+     * 
+     * @return the text
+     */
+    public String text() {
+        return field.getText();
+    }
+
+    /**
+     * Sets the horizontal alignment of the text in the inner text field.
+     * 
+     * @param alignment the alignment (e.g., JTextField.LEFT, JTextField.CENTER, JTextField.RIGHT)
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> horizontalAlignment(int alignment) {
+        field.setHorizontalAlignment(alignment);
+        return this;
+    }
+
+    /**
+     * Returns the horizontal alignment of the text in the inner text field.
+     * 
+     * @return the alignment
+     */
+    public int horizontalAlignment() {
+        return field.getHorizontalAlignment();
+    }
+
+    /**
+     * Sets the font of the inner text field.
+     * 
+     * @param font the font
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> font(java.awt.Font font) {
+        field.setFont(font);
+        return this;
+    }
+
+    /**
+     * Returns the font of the inner text field.
+     * 
+     * @return the font
+     */
+    public java.awt.Font font() {
+        return field.getFont();
+    }
+
+    /**
+     * Sets the foreground color of the inner text field.
+     * 
+     * @param color the foreground color
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> foreground(Color color) {
+        field.setForeground(color);
+        return this;
+    }
+
+    /**
+     * Returns the foreground color of the inner text field.
+     * 
+     * @return the foreground color
+     */
+    public Color foreground() {
+        return field.getForeground();
+    }
+
+    /**
+     * Sets the caret position in the inner text field.
+     * 
+     * @param position the position
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> caretPosition(int position) {
+        field.setCaretPosition(position);
+        return this;
+    }
+
+    /**
+     * Returns the caret position in the inner text field.
+     * 
+     * @return the position
+     */
+    public int caretPosition() {
+        return field.getCaretPosition();
+    }
+
+    /**
+     * Sets the selection start in the inner text field.
+     * 
+     * @param position the selection start
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> selectionStart(int position) {
+        field.setSelectionStart(position);
+        return this;
+    }
+
+    /**
+     * Returns the selection start in the inner text field.
+     * 
+     * @return the selection start
+     */
+    public int selectionStart() {
+        return field.getSelectionStart();
+    }
+
+    /**
+     * Sets the selection end in the inner text field.
+     * 
+     * @param position the selection end
+     * @return This DEditChange instance.
+     */
+    public DEditChange<T> selectionEnd(int position) {
+        field.setSelectionEnd(position);
+        return this;
+    }
+
+    /**
+     * Returns the selection end in the inner text field.
+     * 
+     * @return the selection end
+     */
+    public int selectionEnd() {
+        return field.getSelectionEnd();
     }
 
     /**
